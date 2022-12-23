5a5da57734d06332d90e2a6fd19aecf026de3a47
Abhiram Natarajan <abhiram@Abhirams-MacBook-Air.local>
2022-12-22 22:04:55 -0500

    Basic variable and constant declarations and substitutions

--- main.go
+++ main.go
@@ -4,5 +4,8 @@ import "fmt"
 
 // This is the entry point for the file. Need to put things under a function
 func main() {
-	fmt.Println("Test 123")
+	var nameApp = "Go Conference Booking Application" // Compiler error when variable, package is unused!
+	fmt.Println("Welcome to the ", nameApp, ". Get your tickets here.")
+	const totalNumberOfTickets = 50
+
 }
