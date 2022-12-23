4b127229a6ad12369dc73764f5cc5e701b868926
Abhiram Natarajan <abhiram@Abhirams-MacBook-Air.local>
2022-12-22 22:10:41 -0500

    Added some format specifier with placeholders

--- main.go
+++ main.go
@@ -7,5 +7,7 @@ func main() {
 	var nameApp = "Go Conference Booking Application" // Compiler error when variable, package is unused!
 	fmt.Println("Welcome to the ", nameApp, ". Get your tickets here.")
 	const totalNumberOfTickets = 50
-
+	var numberOfTicketsRemaining = 50
+	// fmt.Println("Currently we have ", numberOfTicketsRemaining, "remaining")
+	fmt.Printf("Currently we have %v remaining tickets\n", numberOfTicketsRemaining)
 }
