1c6d54ac31bc2e07fab73cef7b93ae5cac195e16
Abhiram Natarajan <abhiram@Abhirams-MacBook-Air.local>
2022-12-23 00:14:30 -0500

    used functions

--- main.go
+++ main.go
@@ -14,6 +14,55 @@ func main() {
 	// fmt.Println("Currently we have ", numberOfTicketsRemaining, "remaining")
 	fmt.Printf("Currently we have %v remaining tickets\n", numberOfTicketsRemaining)
 
+	// Datatypes processing
+	// basic(&numberOfTicketsRemaining) // Passing by reference
+
+	// For loops:
+	// loops(&numberOfTicketsRemaining)
+
+}
+
+func loops(numberOfTicketsRemaining *int) {
+	// Arrays and slices
+	// var bookings = [50]string{"User1", "User2"} // Size of the array and datatype, can add some default values, else just add {} for empty.
+
+	// Alternative Syntax
+	// var bookings [50]string
+	// Dynamic array size -> Slice; abstraction of array.
+	var bookings []string
+	// Can run these infinite times with a loop = for {} or for some condition
+	for *numberOfTicketsRemaining > 0 {
+		var firstName, lastName string
+		fmt.Println("Enter first and last name")
+		fmt.Scan(&firstName)
+		fmt.Scan(&lastName)
+		// Validate user input
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		if !isValidName {
+			fmt.Println("Invalid input")
+			continue
+		}
+		// bookings[0] = firstName + " " + lastName # This is for array syntax
+		bookings = append(bookings, firstName+" "+lastName)
+		fmt.Printf("First Name %v LastName %v Array Length %v\n", firstName, lastName, len(bookings))
+		// Using a for-each
+		var firstNames []string
+		for _, val := range bookings { // _ used for blank identifiers that we dont want to use.
+			var name = strings.Fields(val) // slices into first and last name based on space
+			firstNames = append(firstNames, name[0])
+		}
+		fmt.Println("The booked users so far are", firstNames)
+
+		// Now should terminate the application if all tickets sold
+		if *numberOfTicketsRemaining == 0 {
+			fmt.Println("All tickets sold out")
+			break
+		}
+		*numberOfTicketsRemaining -= 1
+	}
+}
+
+func basic(numberOfTicketsRemaining *int) {
 	/* Data Types */
 	// Strings, Integers, Booleans, Maps, Arrays
 
@@ -29,39 +78,12 @@ func main() {
 
 	fmt.Println("Enter the number of tickets")
 	fmt.Scan(&numTickets)
-	numberOfTicketsRemaining -= numTickets
-	fmt.Println("Number of tickets left", numberOfTicketsRemaining)
-
-	// Arrays and slices
-	// var bookings = [50]string{"User1", "User2"} // Size of the array and datatype, can add some default values, else just add {} for empty.
-
-	// Alternative Syntax
-	// var bookings [50]string
-
-	// Dynamic array size -> Slice; abstraction of array.
-	var bookings []string
-	// Can run these infinite times with a loop
-	for {
-		var firstName, lastName string
-		fmt.Println("Enter first and last name")
-		fmt.Scan(&firstName)
-		fmt.Scan(&lastName)
-		// bookings[0] = firstName + " " + lastName # This is for array syntax
-		bookings = append(bookings, firstName+" "+lastName)
-		fmt.Printf("First Name %v LastName %v Array Length %v\n", firstName, lastName, len(bookings))
-		// Using a for-each
-		var firstNames []string
-		for _, val := range bookings { // _ used for blank identifiers that we dont want to use.
-			var name = strings.Fields(val) // slices into first and last name based on space
-			firstNames = append(firstNames, name[0])
-		}
-		fmt.Println("The booked users so far are", firstNames)
-
-		// Now should terminate the application if all tickets sold
-		if numberOfTicketsRemaining == 0 {
-			fmt.Println("All tickets sold out")
-			break
-		}
-		numberOfTicketsRemaining -= 1
+	if numTickets > *numberOfTicketsRemaining {
+		fmt.Println("Impossible")
+	} else if numTickets < 0 {
+		fmt.Println("Again, impossible")
+	} else {
+		*numberOfTicketsRemaining -= numTickets
+		fmt.Println("Number of tickets left", *numberOfTicketsRemaining)
 	}
 }
