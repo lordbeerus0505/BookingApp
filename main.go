a7a139771a5c902f6886981697fa8e5601a63563
Abhiram Natarajan <abhiram@Abhirams-MacBook-Air.local>
2022-12-22 22:25:32 -0500

    Added basic arithmetic and variables

--- main.go
+++ main.go
@@ -10,4 +10,23 @@ func main() {
 	var numberOfTicketsRemaining = 50
 	// fmt.Println("Currently we have ", numberOfTicketsRemaining, "remaining")
 	fmt.Printf("Currently we have %v remaining tickets\n", numberOfTicketsRemaining)
+
+	/* Data Types */
+	// Strings, Integers, Booleans, Maps, Arrays
+
+	// Go can infer the datatype when you provide a value, if not; explicitly provide a type.
+	var userName string // variable userName of type String
+	fmt.Scanf("%v", &userName)
+	fmt.Printf("Hello %v\n", userName)
+
+	// To declare a variable - can also use the := syntax
+	var age, numTickets int
+	fmt.Scan(&age)
+	fmt.Println("The user's age is", age)
+
+	fmt.Println("Enter the number of tickets")
+	fmt.Scan(&numTickets)
+	numberOfTicketsRemaining -= numTickets
+	fmt.Println("Number of tickets left", numberOfTicketsRemaining)
+
 }
